43f432fa32a3b922ad8e63ace1aa58a82d6b4d2e
AbdulSaboor56 <saboor1787@gmail.com>
2024-02-02 10:37:31 +0500

    accessing the BRAVO from LaunchBRAVO page

--- src/test/java/signintoBRAVO/BravoLaunch.java
+++ src/test/java/signintoBRAVO/BravoLaunch.java
@@ -15,6 +15,8 @@ public class BravoLaunch {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Coeus\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
+        
+         driver.get("https://app.workhub.ai/admin/login");
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(email, password);
