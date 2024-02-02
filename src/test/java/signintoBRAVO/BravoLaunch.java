72c232ffffbf6ba400b95a90932d53718687b864
AbdulSaboor56 <saboor1787@gmail.com>
2024-02-02 17:18:21 +0500

    headless

--- src/test/java/signintoBRAVO/BravoLaunch.java
+++ src/test/java/signintoBRAVO/BravoLaunch.java
@@ -1,8 +1,8 @@
-
 package signintoBRAVO;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BravoLaunch {
     protected WebDriver driver;
@@ -12,16 +12,15 @@ public class BravoLaunch {
     String email = System.getenv("email");
     String password = System.getenv("password");
 
-
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Coeus\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        driver = new ChromeDriver();
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         
-         driver.get("https://app.workhub.ai/admin/login");
-
+        
+           driver.manage().window().maximize();
+  
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(email, password);
     }
 }
-  
- 
-
